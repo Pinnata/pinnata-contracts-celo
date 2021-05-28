312db02490d4bff1836655a3e4d647472f43bfab
Ian Macalinao <github@igm.pub>
2021-05-27 20:13:52 -0500

    Get some tests to pass

--- setup_deps.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pipx install eth-brownie
-brownie pm install OpenZeppelin/openzeppelin-contracts@3.4.0
-brownie pm install OpenZeppelin/openzeppelin-contracts@3.2.0
--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs }:
-let my-python-packages = python-packages: with python-packages; [ pipx ];
+let my-python-packages = python-packages: with python-packages; [ pipx pip ];
 in pkgs.mkShell {
   buildInputs = with pkgs; [
     (python3.withPackages my-python-packages)
@@ -9,5 +9,7 @@ in pkgs.mkShell {
   ];
   shellHook = ''
     export PATH="$HOME/.local/bin:./node_modules/.bin:$PATH"
+    pipx install eth-brownie
+    pipx install pytest
   '';
 }
