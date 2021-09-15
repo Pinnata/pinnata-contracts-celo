1d714ead6ac5c4224d3f2cf812774370d93e8545
Kyle <kyscott@umich.edu>
2021-09-14 20:58:32 -0400

    safebox deploy

--- scripts/dahlia_deploy_safeboxes.py
+++ scripts/dahlia_deploy_safeboxes.py
@@ -11,37 +11,6 @@ import json
 network.gas_limit(8000000)
 
 def main():
-<<<<<<< HEAD
-    deployer = accounts.load('admin')
-    f = open('scripts/dahlia_addresses.json')
-    addr = json.load(f)['mainnet']
-
-    celo = interface.IERC20Ex(addr['celo'])
-    ube = interface.IERC20Ex(addr['ube'])
-    btc = interface.IERC20Ex(addr['btc'])
-    fcelo = interface.IERC20Ex(addr['fcelo'])
-    fube = interface.IERC20Ex(addr['fube'])
-    fbtc = interface.IERC20Ex(addr['fbtc'])
-    dahlia_bank = HomoraBank.at(addr['dahlia_bank'])
-
-    # # deploy safeboxes
-
-    SafeBox.deploy(
-        fcelo, 'Interest Bearing Celo', 'dCELO', {'from': deployer})
-    SafeBox.deploy(
-        fube, 'Interest Bearing UBE', 'dUBE', {'from': deployer})
-    SafeBox.deploy(
-        fbtc, 'Interest Bearing BTC', 'dBTC', {'from': deployer})
-    
-    # add banks
-    dahlia_bank.addBank(celo, fcelo, {'from': deployer})
-    dahlia_bank.addBank(ube, fube, {'from': deployer})
-    dahlia_bank.addBank(btc, fbtc, {'from': deployer})
-
-    dahlia_bank.setWhitelistTokens([celo], [True], {'from': deployer})
-    dahlia_bank.setWhitelistTokens([ube], [True], {'from': deployer})
-    dahlia_bank.setWhitelistTokens([btc], [True], {'from': deployer})
-=======
     deployer = accounts.load('dahlia_admin')
 
     with open('scripts/dahlia_addresses.json', 'r') as f:
@@ -88,6 +57,5 @@ def main():
         'dube': dube.address,
         'dscelo': dscelo.address,
     })
->>>>>>> kjs/beta_deployment
 
     print(json.dumps(addr, indent=4), file=open('scripts/dahlia_addresses.json', 'w'))
