386b2fa9fb1eb4c51fb728c17c74fa84a9862ac9
Kyle <kyscott@umich.edu>
2021-10-09 10:44:14 -0400

    remove for max

--- scripts/dahlia_remove_bug.py
+++ scripts/dahlia_remove_bug.py
@@ -1,111 +1,53 @@
-from brownie import accounts, interface, Contract
+from os import error
 from brownie import (
-    HomoraBank, ProxyOracle, CoreOracle, UniswapV2Oracle, SimpleOracle, UniswapV2SpellV1, WERC20, SafeBox, WStakingRewards
+    accounts,
+    interface,
+    Contract,
+    HomoraBank,
+    network
 )
 import json
 
+network.gas_limit(8000000)
 
 def main():
-    kyle= accounts.load('kyle_personal')
+    deployer = accounts.load('dahlia_admin')
+    person = accounts.load('dahlia_alice')
     f = open('scripts/dahlia_addresses.json')
     addr = json.load(f)['mainnet']
+    position = 2
 
     celo = interface.IERC20Ex(addr['celo'])
     mcusd = interface.IERC20Ex(addr['mcusd'])
-    dahlia_bank = HomoraBank.at(addr['dahlia_bank'])
-    uniswap_spell = UniswapV2SpellV1.at(addr['uni_spell'])
-    # core_oracle = CoreOracle.at(addr['core_oracle'])
-    celo_safebox = SafeBox.at(addr['dcelo'])
-    mcusd_safebox = SafeBox.at(addr['dmcusd'])
-    wstaking = WStakingRewards.at(addr['celo_mcusd_wstaking'])
+    mceur = interface.IERC20Ex(addr['mceur'])
+    scelo = interface.IERC20Ex(addr['scelo'])
+    ube = interface.IERC20Ex(addr['ube'])
+    fcelo = interface.IERC20Ex(addr['fcelo'])
+    fmcusd = interface.IERC20Ex(addr['fmcusd'])
+    fmceur = interface.IERC20Ex(addr['fmceur'])
+    fscelo = interface.IERC20Ex(addr['fscelo'])
+    fube = interface.IERC20Ex(addr['fube'])
+    dahlia_bank = Contract.from_abi("HomoraBank", addr.get('dahlia_bank'), HomoraBank.abi)
+    # dahlia_bank.setFeeBps(10, {'from': deployer})
 
-    # lend(bob, celo, celo_safebox)
-    # lend(bob, ube, ube_safebox)
+    dahlia_bank.accrue(celo, {'from': person})
+    dahlia_bank.accrue(mcusd, {'from': person})
+    dahlia_bank.accrue(mceur, {'from': person})
+    dahlia_bank.accrue(scelo, {'from': person})
+    dahlia_bank.accrue(ube, {'from': person})
 
-    # celo.approve(dahlia_bank, 2**256-1, {'from': alice})
-    # ube.approve(dahlia_bank, 2**256-1, {'from': alice})
+    (owner, collToken, collId, collateralsize) = dahlia_bank.getPositionInfo(position)
+    collat = dahlia_bank.getCollateralCELOValue(position)
+    borrow = dahlia_bank.getBorrowCELOValue(position)
+    print("collateral value:", collat)
+    print("borrow value:", borrow)
+    print("owner:", owner)
+    print("collToken:", collToken)
+    print("collId:", collId)
+    print("collateral size:", collateralsize)
 
-    # prevABal = celo.balanceOf(alice)
-    # prevBBal = ube.balanceOf(alice)
-
-    # initABal = prevABal
-    # initBBal = prevBBal
-
-    # print('prev A bal', prevABal)
-    # print('prev B bal', prevBBal)
-
-    # open a position
-    # dahlia_bank.execute(
-    #     0,
-    #     uniswap_spell,
-    #     uniswap_spell.addLiquidityWStakingRewards.encode_input(
-    #         celo,
-    #         ube,
-    #         [10**10,
-    #          10**10,
-    #          0,
-    #          0,
-    #          5 * 10**4,
-    #          0,
-    #          0,
-    #          0],
-    #         wstaking
-    #     ),
-    #     {
-    #         'from': alice, 
-    #     }
-    # )
-
-    # curABal = celo.balanceOf(alice)
-    # curBBal = ube.balanceOf(alice)
-
-    # print('alice delta A Bal', curABal - prevABal)
-    # print('alice delta B Bal', curBBal - prevBBal)
-
-    # prevABal = celo.balanceOf(alice)
-    # prevBBal = ube.balanceOf(alice)
-
-    position_id = dahlia_bank.nextPositionId()
-
-    # close the position
-    dahlia_bank.execute(
-        position_id - 1,
-        uniswap_spell,
-        uniswap_spell.removeLiquidityWStakingRewards.encode_input(
-            celo,
-            mcusd,
-            [2**256-1,
-             0,
-             2**256-1,
-             2**256-1,
-             0,
-             0,
-             0],
-            wstaking
-        ),
-        {'from': kyle}
-    )
-
-    # curABal = celo.balanceOf(alice)
-    # curBBal = ube.balanceOf(alice)
-
-    # finalABal = curABal
-    # finalBBal = curBBal
-
-    # tokenAPrice = core_oracle.getCELOPx(celo)
-    # tokenBPrice = core_oracle.getCELOPx(ube)
-
-    # print('alice delta A Bal', curABal - prevABal)
-    # print('alice delta B Bal', curBBal - prevBBal)
-
-    # print('token A price', tokenAPrice)
-    # print('token B price', tokenBPrice)
-
-    # assert almostEqual(tokenAPrice * initABal + tokenBPrice * initBBal,
-    #                    tokenAPrice * finalABal + tokenBPrice * finalBBal), 'too much value lost'
-
-    # celo.approve(dahlia_bank, 0, {'from': alice})
-    # ube.approve(dahlia_bank, 0, {'from': alice})
-
-    # withdraw(bob, celo, celo_safebox)
-    # withdraw(bob, ube, ube_safebox)
+    assert dahlia_bank.getBankInfo(celo)[3] == fcelo.borrowBalanceStored(dahlia_bank.address)
+    assert dahlia_bank.getBankInfo(mcusd)[3] == fmcusd.borrowBalanceStored(dahlia_bank.address)
+    assert dahlia_bank.getBankInfo(mceur)[3] == fmceur.borrowBalanceStored(dahlia_bank.address)
+    assert dahlia_bank.getBankInfo(scelo)[3] == fscelo.borrowBalanceStored(dahlia_bank.address)
+    assert dahlia_bank.getBankInfo(ube)[3] == fube.borrowBalanceStored(dahlia_bank.address)
