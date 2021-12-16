9912a280d09f032c74b6d262e5ce21a152025859
Kyle <kyscott@umich.edu>
2021-12-16 16:31:42 -0500

    compile dia oracle

---
+++ contracts/oracle/DiaAdapterOracle.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.6.12;
+
+import 'OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/math/SafeMath.sol';
+import '../Governable.sol';
+import '../../interfaces/IBaseOracle.sol';
+import './DIAOracle.sol';
+
+contract DiaAdapterOracle is IBaseOracle, Governable {
+  using SafeMath for uint256;
+
+  address public diaOracle;
+  uint256 public maxDelay;
+  mapping(address => string) private query;
+
+  constructor (address _diaOracle, uint256 _maxDelay) public {
+    __Governable__init();
+    diaOracle = diaOracle;
+    maxDelay = _maxDelay;
+  }
+
+  function setMaxDelay(uint256 _maxDelay) external onlyGov {
+    maxDelay = _maxDelay;
+  }
+
+  function setQuery(address token, string calldata key) external onlyGov {
+    query[token] = key;
+  }
+
+  /// @dev Return the value of the given input as CELO per unit, multiplied by 2**112.
+  /// @param token The ERC-20 token to check the value.
+  function getCELOPx(address token) external view override returns (uint) {
+    (uint128 celoPrice, uint128 celoTimestamp) = DIAOracle(diaOracle).getValue("CELO/USD");
+    // require(celoTimestamp >= block.timestamp.sub(maxDelay), "delayed celo update time");
+    return 1;
+    // (uint128 price, uint128 timestamp) = diaOracle.getValue(query[token]);
+    // require(timestamp >= block.timestamp.sub(maxDelay), "delayed update time");
+    // return uint(price).mul(2**112).div(celoPrice);
+  }
+}
---
+++ scripts/dahlia_test_dia_oracle.py
@@ -0,0 +1,21 @@
+from brownie import (
+    DiaAdapterOracle,
+    accounts,
+    network,
+)
+import json
+
+network.gas_limit(8000000)
+
+def main():
+    deployer = accounts.load('dahlia_alice')
+
+    f = open('scripts/dahlia_addresses.json')
+    addr = json.load(f)['mainnet']
+    ube = addr['ube']
+    print(ube)
+    dia_oracle = DiaAdapterOracle.deploy('0x7D1e0D8b0810730e85828EaE1ee1695a95eECf4B', 180, {'from': deployer})
+
+    # dia_oracle.setQuery(addr['ube'], 'UBE/USD', {'from': deployer})
+
+    # print('ube', dia_oracle.getCELOPx(ube))
