511b6db28a6339982d50bf1d5d62f3bde245d718
Dylan Mooers <dylmooers@gmail.com>
2021-12-28 23:53:15 -0800

    modified WLiquidityGauge to work with Mobius

--- contracts/wrapper/WLiquidityGauge.sol
+++ contracts/wrapper/WLiquidityGauge.sol
@@ -9,7 +9,6 @@ import 'OpenZeppelin/openzeppelin-contracts@3.4.0/contracts/utils/ReentrancyGuar
 import '../Governable.sol';
 import '../utils/HomoraMath.sol';
 import '../../interfaces/IERC20Wrapper.sol';
-import '../../interfaces/ICurveRegistry.sol';
 import '../../interfaces/ILiquidityGauge.sol';
 
 interface ILiquidityGaugeMinter {
@@ -24,54 +23,50 @@ contract WLiquidityGauge is ERC1155('WLiquidityGauge'), ReentrancyGuard, IERC20W
   struct GaugeInfo {
     ILiquidityGauge impl; // Gauge implementation
     uint accMobiPerShare; // Accumulated MOBI per share
+    uint id; // Pool Id
   }
 
-  ICurveRegistry public immutable registry; // Curve registry
   IERC20 public immutable mobi; // MOBI token
-  mapping(uint => mapping(uint => GaugeInfo)) public gauges; // Mapping from pool id to (mapping from gauge id to GaugeInfo)
+  uint nextPoolId; // The next pool id to assign
+  mapping(address => GaugeInfo) public gauges; // Mapping from pool id to (mapping from gauge id to GaugeInfo)
+  mapping(uint => GaugeInfo) public idToGauges; // Mapping from pool id to Gauge Info
 
-  constructor(ICurveRegistry _registry, IERC20 _mobi) public {
+  constructor(IERC20 _mobi) public {
     __Governable__init();
-    registry = _registry;
     mobi = _mobi;
+    nextPoolId = 0;
   }
 
-  /// @dev Encode pid, gid, mobiPerShare to a ERC1155 token id
+  /// @dev Encode pid, mobiPerShare to a ERC1155 token id
   /// @param pid Curve pool id (10-bit)
-  /// @param gid Curve gauge id (6-bit)
   /// @param mobiPerShare MOBI amount per share, multiplied by 1e18 (240-bit)
   function encodeId(
     uint pid,
-    uint gid,
     uint mobiPerShare
   ) public pure returns (uint) {
     require(pid < (1 << 10), 'bad pid');
-    require(gid < (1 << 6), 'bad gid');
     require(mobiPerShare < (1 << 240), 'bad mobi per share');
-    return (pid << 246) | (gid << 240) | mobiPerShare;
+    return (pid << 240) | mobiPerShare;
   }
 
-  /// @dev Decode ERC1155 token id to pid, gid, mobiPerShare
+  /// @dev Decode ERC1155 token id to pid, mobiPerShare
   /// @param id Token id to decode
   function decodeId(uint id)
     public
     pure
     returns (
       uint pid,
-      uint gid,
       uint mobiPerShare
     )
   {
-    pid = id >> 246; // First 10 bits
-    gid = (id >> 240) & (63); // Next 6 bits
+    pid = id >> 240; // First 16 bits
     mobiPerShare = id & ((1 << 240) - 1); // Last 240 bits
   }
 
-  /// @dev Get underlying ERC20 token of ERC1155 given pid, gid
+  /// @dev Get underlying ERC20 token of ERC1155 given pid
   /// @param pid pool id
-  /// @param gid gauge id
-  function getUnderlyingTokenFromIds(uint pid, uint gid) public view returns (address) {
-    ILiquidityGauge impl = gauges[pid][gid].impl;
+  function getUnderlyingTokenFromId(uint pid) public view returns (address) {
+    ILiquidityGauge impl = idToGauges[pid].impl;
     require(address(impl) != address(0), 'no gauge');
     return impl.lp_token();
   }
@@ -79,8 +74,8 @@ contract WLiquidityGauge is ERC1155('WLiquidityGauge'), ReentrancyGuard, IERC20W
   /// @dev Get underlying ERC20 token of ERC1155 given token id
   /// @param id Token id
   function getUnderlyingToken(uint id) external view override returns (address) {
-    (uint pid, uint gid, ) = decodeId(id);
-    return getUnderlyingTokenFromIds(pid, gid);
+    (uint pid,, ) = decodeId(id);
+    return getUnderlyingTokenFromIds(pid);
   }
 
   /// @dev Return the conversion rate from ERC-1155 to ERC-20, multiplied by 2**112.
@@ -88,39 +83,34 @@ contract WLiquidityGauge is ERC1155('WLiquidityGauge'), ReentrancyGuard, IERC20W
     return 2**112;
   }
 
-  /// @dev Register curve gauge to storage given pool id and gauge id
-  /// @param pid Pool id
-  /// @param gid Gauge id
-  function registerGauge(uint pid, uint gid) external onlyGov {
-    require(address(gauges[pid][gid].impl) == address(0), 'gauge already exists');
-    address pool = registry.pool_list(pid);
-    require(pool != address(0), 'no pool');
-    (address[10] memory _gauges, ) = registry.get_gauges(pool);
-    address gauge = _gauges[gid];
-    require(gauge != address(0), 'no gauge');
-    IERC20 lpToken = IERC20(ILiquidityGauge(gauge).lp_token());
-    lpToken.approve(gauge, 0);
-    lpToken.approve(gauge, uint(-1));
-    gauges[pid][gid] = GaugeInfo({impl: ILiquidityGauge(gauge), accMobiPerShare: 0});
+  /// @dev Register gauge to storage given pool address and gauge address
+  /// @param poolAddress Pool address
+  /// @param gaugeAddress Gauge address
+  function registerGauge(address poolAddress, address gaugeAddress) external onlyGov {
+    require(address(gauges[poolAddress].impl) == address(0), 'gauge already exists');
+    IERC20 lpToken = IERC20(ILiquidityGauge(gaugeAddress).lp_token());
+    lpToken.approve(gaugeAddress, 0);
+    lpToken.approve(gaugeAddress, uint(-1));
+    GaugeInfo memory info = GaugeInfo({impl: ILiquidityGauge(gaugeAddress), accMobiPerShare: 0, id: nextPoolId});
+    gauges[poolAddress] = info;
+    idToGauges[nextPoolId++] = info;
   }
 
   /// @dev Mint ERC1155 token for the given ERC20 token
-  /// @param pid Pool id
-  /// @param gid Gauge id
+  /// @param poolAddress Pool address
   /// @param amount Token amount to wrap
   function mint(
-    uint pid,
-    uint gid,
+    address poolAddress,
     uint amount
   ) external nonReentrant returns (uint) {
-    GaugeInfo storage gauge = gauges[pid][gid];
+    GaugeInfo storage gauge = gauges[poolAddress];
     ILiquidityGauge impl = gauge.impl;
     require(address(impl) != address(0), 'gauge not registered');
     mintMobi(gauge);
     IERC20 lpToken = IERC20(impl.lp_token());
     lpToken.safeTransferFrom(msg.sender, address(this), amount);
     impl.deposit(amount);
-    uint id = encodeId(pid, gid, gauge.accMobiPerShare);
+    uint id = encodeId(gauge.id, gauge.accMobiPerShare);
     _mint(msg.sender, id, amount, '');
     return id;
   }
@@ -132,9 +122,9 @@ contract WLiquidityGauge is ERC1155('WLiquidityGauge'), ReentrancyGuard, IERC20W
     if (amount == uint(-1)) {
       amount = balanceOf(msg.sender, id);
     }
-    (uint pid, uint gid, uint stMobiPerShare) = decodeId(id);
+    (uint pid, uint stMobiPerShare) = decodeId(id);
     _burn(msg.sender, id, amount);
-    GaugeInfo storage gauge = gauges[pid][gid];
+    GaugeInfo storage gauge = idToGauges[pid];
     ILiquidityGauge impl = gauge.impl;
     require(address(impl) != address(0), 'gauge not registered');
     mintMobi(gauge);
